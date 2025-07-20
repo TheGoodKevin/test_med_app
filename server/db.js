4703696594f85bacb810955897f6b33079291289
kevin shreenauth <kevinshreenauth@gmail.com>
2025-07-19 20:22:30 -0400

    mongo

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<xNjoVtCOHt51wT3wq0TBnYxL>@172.21.245.33:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
