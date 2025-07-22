6297ddefdb94b2bb519d28748432cf62e462a9c8
kevin shreenauth <kevinshreenauth@gmail.com>
2025-07-22 15:48:12 -0400

    Landing Page React Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-// Import necessary modules from React library
 import React from 'react';
-
-// Import components for routing from react-router-dom library
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import custom Navbar component
 import Navbar from './Components/Navbar/Navbar';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
 
-// Function component for the main App
 function App() {
-
-  // Render the main App component
   return (
     <div className="App">
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */}
-          <Navbar/>
-
-          {/* Set up the Routes for different pages */}
-          <Routes>
-            {/* Define individual Route components for different pages */}
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing_Page />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-// Export the App component as the default export
 export default App;
